12ae52117478f62ba96dad1811723195669ab503
ManfredGit <manfred.hu@hotmail.com>
2019-03-24 23:46:15 -0400

    add some new String operations

--- Java8ExamplesRepo/Java8Examples/src/Java8Samples/StringTest.java
+++ Java8ExamplesRepo/Java8Examples/src/Java8Samples/StringTest.java
@@ -13,5 +13,21 @@ public class StringTest {
 		StringBuffer sb2 =  sb1;
 		sb1.append("d");
 		System.out.println(sb1+"  "+sb2+" "+(sb1==sb2));
+		
+		System.out.println("Test?# is reaplced to "+specialCharacter("Test?#"));
+		
+		equalsOperator();
+	}
+	
+	private static String specialCharacter(String str){
+		//(valid ones are  \b  \t  \n  \f  \r  \"  \'  \\ )
+		return str.replaceAll("\\?", "[a-z]").replaceAll("\\#", "[0-9]");
+	}
+	
+	private static void equalsOperator() {
+		String s1 = new String("Test");
+		String s2 = new String("Test");
+		System.out.println(s1 == s2);
+		System.out.println(s1.equals(s2));
 	}
 }
